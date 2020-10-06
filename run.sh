815f9d9cfaae432d771a0e0ab5ee4f47716c963f
Ubuntu <ubuntu@ip-172-31-6-209.ec2.internal>
2020-10-06 14:53:44 +0000

    new run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+
+tmux
+source ~/env/bin/activate
+pip install -r requirements.txt
+chmod +x main_cl.py main_pretrain.py compare_both.py
+./compare_both.py --lambda-500=10000 --lambda=10000 --o-lambda-500=10000 --o-lambda=1000 --c-500=10000 --c=100 --n-seeds=4 --reinit --slot=3 --shift=4
+git add .
+git commit -m "res for slot 3 shift 4"
+git push
+sudo shutdown now
