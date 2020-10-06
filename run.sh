79d4d4718cead20cad6a6affd3f4ff33051bdb66
Ubuntu <ubuntu@ip-172-31-3-215.ec2.internal>
2020-10-06 18:24:05 +0000

    res for slot 8 shift 2

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 source ~/env/bin/activate
 pip install -r requirements.txt
 chmod +x main_cl.py main_pretrain.py compare_both.py
-./compare_both.py --lambda-500=10000 --lambda=10000 --o-lambda-500=10000 --o-lambda=1000 --c-500=10000 --c=100 --n-seeds=4 --reinit --slot=6 --shift=5
+./compare_both.py --lambda-500=10000 --lambda=10000 --o-lambda-500=10000 --o-lambda=1000 --c-500=10000 --c=100 --n-seeds=4 --reinit --slot=8 --shift=5
 git add ./store/results
-git commit -m "res for slot 6 shift 5"
+git commit -m "res for slot 8 shift 5"
 git push
 sudo shutdown now
