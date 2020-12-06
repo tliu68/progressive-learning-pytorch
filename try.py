13a06c7eded8061211422ad3b45e838497756b95
j1c <jdey4@jhmi.edu>
2020-12-06 03:40:46 -0500

    res

--- try.py
+++ try.py
@@ -3,46 +3,32 @@ import numpy as np
 import pickle
 import pandas as pd
 #%%
-with open('./store/results/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i10-lr0.0001-b256--EWC10000.0-1000-O1-1.pkl', 'rb') as f:
+with open('/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i2500-lr0.0001-b256--EWC10000.0-1000-s4-64.pkl', 'rb') as f:
     data = pickle.load(f)
 
 print(data)
 # %%
-file_to_process = "./store/results/dict-CIFAR100-N10-max50--C3-5x16-bn_F-1024x2000x2000_c100--i500-lr0.0001-b256--EWC10000.0-1000-"
-slots = range(1,11)
-shifts = range(1,7)
+file_to_process = "/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i2500-lr0.0001-b256--SI10000.0-0.1"
+reps = 10
+angles = range(0,184,4)
 
-for shift in shifts:
-    for slot in slots:
-        multitask_df = pd.DataFrame()
-        df_single_task = pd.DataFrame()
-        shft = []
-        base_task = []
-        task = []
-        accuracy = []
+for angle in angles:
+    accuracy = np.zeros(2,dtype=float)
 
-        filename = file_to_process + str(slot) + '-' + str(shift) +'.pkl'
+    for rep in range(reps):
+        if rep == 0:
+            filename = file_to_process + '-' + str(angle) +'.pkl'
+        else:
+            filename = file_to_process + '-s'+ str(rep) + '-' + str(angle) +'.pkl'
 
         with open(filename, 'rb') as f:
             data = pickle.load(f)['R']
 
-        for ii in range(10):
-            for jj in range(ii+1):
-                shft.append(shift)
-                base_task.append(ii+1)
-                task.append(jj+1)
-                accuracy.append(data['task {}'.format(jj+1)].iloc[ii+1])
+        accuracy[0] += data['task 1'].iloc[1]
+        accuracy[1] += data['task 1'].iloc[2]
+        #print(data['task 1'].iloc[1] , data['task 1'].iloc[2])
 
-        multitask_df['data_fold'] = shft
-        multitask_df['task'] = task
-        multitask_df['base_task'] = base_task
-        multitask_df['accuracy'] = accuracy
-        
-        df_single_task['task'] = range(1, 11)
-        df_single_task['data_fold'] = shift
-        df_single_task['accuracy'] = list(data.iloc[11])
-
-        summary = (multitask_df,df_single_task)
-        with open('./reformed_res/EWC-{}-{}.pickle'.format(slot, shift), 'wb') as f:
-            pickle.dump(summary, f)
+    accuracy /= reps   
+    with open('./reformed_res/si-{}.pickle'.format(angle), 'wb') as f:
+        pickle.dump(1-accuracy, f)
 # %%
