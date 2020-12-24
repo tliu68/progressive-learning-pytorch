1efcb2ea825e9e87e66487da2368828fb2665317
j1c <jdey4@jhmi.edu>
2020-12-24 09:28:00 -0600

    file coversion

--- try.py
+++ try.py
@@ -2,13 +2,15 @@
 import numpy as np
 import pickle
 import pandas as pd
+import os
 #%%
-with open('/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i2500-lr0.0001-b256--EWC10000.0-1000-s4-64.pkl', 'rb') as f:
+with open('/Users/jayantadey/slide_res_paper/data/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i500-lr0.0001-b256--EWC10000.0-1000-0.pkl', 'rb') as f:
     data = pickle.load(f)
 
 print(data)
 # %%
-file_to_process = "/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i2500-lr0.0001-b256--SI10000.0-0.1"
+file_to_process1 = "/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i500-lr0.0001-b256"
+file_to_process2 = "/Users/jayantadey/slide_res_paper/angle/dict-CIFAR100-N2-max250--C3-5x16-bn_F-1024x2000x2000_c20--i2500-lr0.0001-b256"
 reps = 10
 angles = range(0,184,4)
 
@@ -17,9 +19,16 @@ for angle in angles:
 
     for rep in range(reps):
         if rep == 0:
-            filename = file_to_process + '-' + str(angle) +'.pkl'
+            filename = file_to_process1 + '-' + str(angle) +'.pkl'
+
+            if not os.path.exists(filename):
+                filename = file_to_process2 + '-' + str(angle) +'.pkl'
+
         else:
-            filename = file_to_process + '-s'+ str(rep) + '-' + str(angle) +'.pkl'
+            filename = file_to_process1 + '-s'+ str(rep) + '-' + str(angle) +'.pkl'
+
+            if not os.path.exists(filename):
+                filename = file_to_process2 + '-s'+ str(rep) + '-' + str(angle) +'.pkl'
 
         with open(filename, 'rb') as f:
             data = pickle.load(f)['R']
@@ -29,6 +38,6 @@ for angle in angles:
         #print(data['task 1'].iloc[1] , data['task 1'].iloc[2])
 
     accuracy /= reps   
-    with open('./reformed_res/si-{}.pickle'.format(angle), 'wb') as f:
+    with open('./reformed_res/none-{}.pickle'.format(angle), 'wb') as f:
         pickle.dump(1-accuracy, f)
 # %%
