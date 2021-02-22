642e060362c5944f7241499935c3d4de55626160
tliu68 <54865879+tliu68@users.noreply.github.com>
2021-02-22 14:22:08 +0800

    Update main_cl.py

--- main_cl.py
+++ main_cl.py
@@ -87,7 +87,7 @@ def run(args, verbose=False):
         max_samples=args.max_samples
     )
     pickle_out = open("get_multitask_experiment.pickle", "wb")
-    pickle.dump((train_datasets, test_datasets, train_datasets.data.numpy()), pickle_out)
+    pickle.dump((train_datasets[0], test_datasets[0], train_datasets[0].data.numpy()), pickle_out)
     pickle_out.close()
 
 
