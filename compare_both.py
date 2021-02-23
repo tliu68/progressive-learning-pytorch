0c194fe741c3356422d5718183cbdcf028cf122c
tliu68 <54865879+tliu68@users.noreply.github.com>
2021-02-24 01:33:05 +0800

    Update compare_both.py

--- compare_both.py
+++ compare_both.py
@@ -358,7 +358,6 @@ if __name__ == '__main__':
                 BTEs_this_alg.append(BTEs_this_alg_this_seed)
                 FTEs_this_alg.append(FTEs_this_alg_this_seed)
                 TEs_this_alg.append(TEs_this_alg_this_seed)
-        print(BTEsp)
         BTEsp.append(calc_mean_bte(BTEs_this_alg, task_num=args.tasks, reps=len(seed_list)))
         FTEsp.append(calc_mean_te(FTEs_this_alg))
         TEsp.append(calc_mean_te(TEs_this_alg))
@@ -368,14 +367,11 @@ if __name__ == '__main__':
     #                                    bottom_title="5000 training samples per task",
     #                                    short_names=short_names, task_num=args.tasks, y_lim=(0.58, 1.32),
     #                                    colors=colors)
-    print(BTEsp, 'BTEsp')
-    print(FTEsp, 'FTEsp')
-    print(TEsp, 'ETsp')
+
     param_stamp = get_param_stamp_from_args(args)
-    # output_file = open("{}/BTEsp-{}-{}.txt".format(args.r_dir, param_stamp, args.angle), 'w')
-    pickle_out = open("{}/all_TEsp-{}-{}.txt".format(args.r_dir, param_stamp, args.angle), "wb")
-    pickle.dump([BTEsp, FTEsp, TEsp], pickle_out)
-    pickle_out.close()
+    output_file = open("{}/BTEsp-{}-{}.txt".format(args.r_dir, param_stamp, args.angle), 'w')
+    output_file.write('{}\n'.format([BTEsp, FTEsp, TEsp]))
+    output_file.close()
     # figure_list.append(figure)
 
 
@@ -387,4 +383,4 @@ if __name__ == '__main__':
     # pp.close()
 
     # Print name of generated plot on screen
-    print("\nGenerated plot: {}/{}.pdf\n".format(args.p_dir, plot_name))
+    # print("\nGenerated plot: {}/{}.pdf\n".format(args.p_dir, plot_name))
